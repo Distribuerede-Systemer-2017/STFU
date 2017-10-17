24b30fe750fb97c00f808e0de2da4cf2634471c0
EmilTP <emto16ad@student.cbs.dk>
2017-10-17 11:54:23 +0200

    CreateStudent done

--- src/main/server/Utility/DBmanager.java
+++ src/main/server/Utility/DBmanager.java
@@ -1,8 +1,12 @@
 package main.server.Utility;
 
+import main.server.models.Student;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
 
 public class DBmanager {
 
@@ -20,4 +24,35 @@ public class DBmanager {
     public static Connection getConnection() {
         return connection;
     }
+
+    //Adding a Student to the database. Receiving a Student object
+    public boolean addStudent (Student student) throws Exception {
+
+        PreparedStatement addStudentStatement =
+                connection.prepareStatement("INSERT INTO Students (idStudent, FirstName, LastName, Password) VALUES (?, ?, ?, ?)");
+
+        try {
+            addStudentStatement.setInt(1, student.getIdStudent());
+            addStudentStatement.setString(2, student.getFirstName());
+            addStudentStatement.setString(3, student.getLastName());
+            addStudentStatement.setString(4, student.getPassword());
+
+            int rowsAffected = addStudentStatement.executeUpdate();
+            if (rowsAffected == 1) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
+
+
+    }
+
+
+
 }
--- src/main/server/controllers/StudentController.java
+++ src/main/server/controllers/StudentController.java
@@ -1,16 +1,18 @@
 package main.server.controllers;
 
 
+import com.google.gson.Gson;
+import main.server.Utility.DBmanager;
 import main.server.models.Student;
 
 public class StudentController {
 
-    public addStudent (String data) throws Exception {
+    public boolean addStudent (String data) throws Exception {
 
         Student student = Gson.fromjson(data, Student.class);
         String hashedPassword = Digester.hashWithSalt(student.getPassword());
         student.setPassword(hashedPassword);
-        return db.addStudent(student);
+        return DBmanager.addStudent(student);
 
     }
 
