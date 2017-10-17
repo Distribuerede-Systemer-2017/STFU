645d1429bb1932d61e6702c0875d1e10e4b80627
EmilTP <emto16ad@student.cbs.dk>
2017-10-17 15:27:17 +0200

    fix merge conflict

--- src/main/server/endpoints/EventEndpoint.java
+++ src/main/server/endpoints/EventEndpoint.java
@@ -1,6 +1,5 @@
 package main.server.endpoints;
 
-<<<<<<< HEAD
 
 import com.google.gson.Gson;
 import main.server.controllers.EventController;
@@ -10,11 +9,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-=======
+
 import main.server.controllers.EventController;
 
 import javax.xml.ws.Response;
->>>>>>> 1bcaecf15533c9af30afeba867c08edc647f5f99
 import java.awt.*;
 import java.sql.SQLException;
 
@@ -24,10 +22,9 @@ public class EventEndpoint {
     @POST
     @Produces("application/json")
     public Response create(String data) throws SQLException {
-<<<<<<< HEAD
         Event event = new Gson().fromJson(data, Event.class);
 
-        if (EventController.addEvent (event)) {
+        if (EventController.addEvent(event)) {
 
             return Response
                     .status(200)
@@ -40,29 +37,7 @@ public class EventEndpoint {
                 .build();
 
     }
-=======
-        String s = new Gson().fromJson(data.String.class);
-
-        if (EventController.addEvent()){
-
-            return Response;
-                    .status(200)
-                    .entity("{\message\":Succes! Event created\"}")
-                    .build();
-
-        }
-else return Response
-        .status(400)
-        .entity ("{\message\":\Failed.\"}")
-        .build();
-
-    }
-
-
-
-
-
->>>>>>> 1bcaecf15533c9af30afeba867c08edc647f5f99
 
 }
+
 
